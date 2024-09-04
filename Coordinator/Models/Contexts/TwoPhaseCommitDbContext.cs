3cf174a5d697b64a3d8d6fa9bfecb5f1519c9cbf
Furkan Mert Mısırlı <misirlifu@mef.edu.tr>
2024-09-04 14:40:45 +0300

    init project

---
+++ Coordinator/Models/Contexts/TwoPhaseCommitDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Coordinator.Models.Contexts
+{
+    public class TwoPhaseCommitDbContext : DbContext
+    {
+        public TwoPhaseCommitDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Node> Nodes { get; set; }
+
+        public DbSet<NodeState> NodeStates { get; set; }
+
+
+    }
+}
---
+++ Coordinator/Program.cs
@@ -0,0 +1,33 @@
+using Coordinator.Models.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<TwoPhaseCommitDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSql")));
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
